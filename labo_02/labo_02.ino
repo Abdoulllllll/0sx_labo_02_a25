6fc0328bcd17684769ea8a306ad17996763c67e8
unknown <watttabd@gmail.com>
2025-02-21 15:46:23 -0500

    dernier commit

--- labo_02/labo_02.ino
+++ labo_02/labo_02.ino
@@ -1,10 +1,14 @@
 unsigned long currentTime = 0;
 
 void setup() {
-  Serial.begin(9600);
-
+  
   // Ton code ici
-
+Serial.begin(9600);
+ pinMode(boutonPin, INPUT_PULLUP); 
+    for (int i = 0; i < 4; i++) {
+        pinMode(leds[i], OUTPUT);
+        digitalWrite(leds[i], LOW);
+    }
 
 
   Serial.println("Setup completed");
@@ -12,5 +16,81 @@ void setup() {
 
 void loop() {
   currentTime = millis();
+unsigned long currentMillis = millis();
 
+    
+    int valeurPot = analogRead(potPin);
+    int progression = map(valeurPot, 0, 1023, 0, 20);
+
+    
+    bool etatBouton = digitalRead(boutonPin);
+    if (etatBouton == LOW and dernierEtatBouton == HIGH) { 
+        boutonAppuye = !boutonAppuye; 
+        afficherBarreProgression(progression);
+    }
+    dernierEtatBouton = etatBouton; 
+
+    
+    if (boutonAppuye and (currentMillis - previousMillis >= interval)) {
+        previousMillis = currentMillis;
+    }
+
+    allumerLEDs(progression);
 }
+
+
+
+
+
+void afficherBarreProgression(int progression) {
+  
+
+    char affichage = '@'; 
+
+    
+    if (progression < 5) Serial.print(" 25% ");
+    else if (progression < 10) Serial.print(" 50% ");
+    else if (progression < 15) Serial.print(" 75% ");
+    else Serial.print("100% ");
+
+    Serial.print("[");
+
+   
+    for (int i = 0; i < 20; i++) {
+        if (i < progression) Serial.print(affichage);
+        else Serial.print(".");
+    }
+
+    Serial.println("]");
+}
+
+
+void allumerLEDs(int progression) {
+    for (int i = 0; i < 4; i++) {
+        digitalWrite(leds[i], LOW);
+    }
+
+    
+    if (progression >= 0 and progression < 5) digitalWrite(leds[0], HIGH);
+    if (progression >= 5 and progression < 10) {
+        digitalWrite(leds[0], HIGH);
+        digitalWrite(leds[1], HIGH);
+    }
+    if (progression >= 10 and progression < 15) {
+        digitalWrite(leds[0], HIGH);
+        digitalWrite(leds[1], HIGH);
+        digitalWrite(leds[2], HIGH);
+    }
+    if (progression >= 15) {
+        digitalWrite(leds[0], HIGH);
+        digitalWrite(leds[1], HIGH);
+        digitalWrite(leds[2], HIGH);
+        digitalWrite(leds[3], HIGH);
+    }
+}
+
+
+
+
+
+
